3e822ea805bf18b4cf845f86e8224ccab79bd501
shahabnoori <noorigoodarzi@gmail.com>
2020-06-18 09:17:17 +0430

    my commit

---
+++ MyProj/MyProj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
